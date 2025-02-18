2d58e8b2464d7c84b2645a314ef267ab9a6d6473
MaksimNK <maksmozir@gmail.com>
2025-02-18 10:49:03 +0300

    update config

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,19 @@
 export default {
-    preset: "ts-jest",
-    testEnvironment: 'jest-fixed-jsdom',
-    extensionsToTreatAsEsm: ['.tsx'],
-    transform: {
-      "^.+\\.tsx?$": [
-        "ts-jest",
-        {
-          tsconfig: "tsconfig.app.json",
-        },
-      ],
-    },
-    moduleNameMapper: {
-      "\\.(css|less|scss|sass)$": "<rootDir>/test/__mocks__/fileMock.js",
-      "\\.(gif|ttf|eot|svg|png|jpg)$": "<rootDir>/test/__mocks__/fileMock.js",
-    },
-    setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
-    collectCoverageFrom: [
-      "src/**/*.{ts,tsx}",
-      "!src/App.tsx"
+  preset: 'ts-jest',
+  testEnvironment: 'jest-fixed-jsdom',
+  extensionsToTreatAsEsm: ['.tsx'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.app.json',
+      },
     ],
-  };
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/App.tsx'],
+};
